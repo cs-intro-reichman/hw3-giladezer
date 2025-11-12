8904a2643b24e367b13e6b73405324999ab06434
Gilad Ezer <gilad.ezer@post.runi.ac.il>
2025-11-12 19:23:31 +0200

    done

--- LoanCalc.java
+++ LoanCalc.java
@@ -12,7 +12,7 @@ public class LoanCalc {
 		double loan = Double.parseDouble(args[0]);
 		double rate = Double.parseDouble(args[1]);
 		int n = Integer.parseInt(args[2]);
-		System.out.println("Loan = " + loan + ", interest rate = " + rate + "%, periods = " + n);
+		System.out.println("Loan = " + loan + ", interest rate = " + rate + "%, periods " + n);
 
 		// Computes the periodical payment using brute force search
 		System.out.print("\nPeriodical payment, using brute force: ");
